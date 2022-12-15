490bcf1fbabac1b2f218fddf01b6a43a59a4ab9c
shivprajapat <shivkumarprajapat232@gmail.com>
2022-12-15 15:35:46 +0530

    redux-parsist use

--- src/store.js
+++ src/store.js
@@ -1,5 +1,16 @@
 import { legacy_createStore as createStore} from 'redux'
+import { persistStore, persistReducer } from 'redux-persist'
+import storage from 'redux-persist/lib/storage' // defaults to localStorage for web
 import rootReducers from "./redux/rootReducer";
 
-const store = createStore(rootReducers)
+ 
+const persistConfig = {
+    key: 'root',
+    storage,
+  }
+  const persistedReducer = persistReducer(persistConfig, rootReducers)
+
+const store = createStore(persistedReducer,window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
+export const persistor = persistStore(store)
+
 export default store;
